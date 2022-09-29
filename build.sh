7d14e54c89753d84641360541e8e046764bb823c
Tom Oliveira <washington-kd@hotmail.com>
2022-09-28 23:44:57 -0300

    modified:   build.sh modified:   main.cpp

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-#conan profile new default --detect
-conan profile update settings.compiler.libcxx=libstdc++11 default
-conan install .
-cmake . -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
-make
+#conan profile new default --detect && \
+conan profile update settings.compiler.libcxx=libstdc++11 default && \
+conan install . && \
+cmake . -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release && \
+make && \
+./bin/fileMain
--- main.cpp
+++ main.cpp
@@ -5,19 +5,21 @@ int main(int argc, char **argv)
 {
 
        sqlite3 *db;
-       char *zErrMsg = 0;
        int rc;
 
        rc = sqlite3_open("test.db", &db);
 
        if (rc)
-       {
-              fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+       {      
+
+              std::cout << "database not opened" << std::endl;
+         
               return (0);
        }
        else
        {
-              fprintf(stderr, "Opened database successfully\n");
+
+              std::cout << "database opened" << std::endl;
        }
        sqlite3_close(db);
        return 0;
